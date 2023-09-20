120d06223c2efb4a13255c504c32b6d1651de4df
Myles <Myleshosken@hotmail.com>
2023-09-20 22:27:24 +1200

    Update articles function

--- backend/api/articles/index.js
+++ backend/api/articles/index.js
@@ -3,11 +3,17 @@ import { ArticleModel } from '../../src/articles/article.model';
 
 export default async (req, res) => {
     console.log("Function invoked!");
-    if (req.method === 'GET') {
-      const db = await connectToDatabase();
-      const articles = await ArticleModel.find();
-      res.json(articles);
-    } else {
-      res.status(405).send('Method Not Allowed');
+
+    try {
+      if (req.method === 'GET') {
+        const db = await connectToDatabase();
+        const articles = await ArticleModel.find();
+        res.json(articles);
+      } else {
+        res.status(405).send('Method Not Allowed');
+      }
+    } catch (error) {
+      console.error("Error during function execution:", error);
+      res.status(500).send('Internal Server Error');
     }
-  };
+};
