c8cb1bd76fc61136f56a7420b7757788a5315e7a
CarrotInPot <vidalfan11@gmail.com>
2023-09-26 15:45:36 +1300

    Moderator view
    implemented a button and separate page for the moderator dashboard. Currently its just the same as the user view.

---
+++ frontend/src/components/table/ModeratorSortableTable.tsx
@@ -0,0 +1,30 @@
+// src/components/table/ModeratorSortableTable.tsx
+import React from "react";
+
+interface ModeratorSortableTableProps {
+    headers: { key: string; label: string }[];
+    data: any[];
+}
+
+const ModeratorSortableTable: React.FC<ModeratorSortableTableProps> = ({ headers, data }) => (
+    <table>
+        <thead>
+            <tr>
+                {headers.map((header) => (
+                    <th key={header.key}>{header.label}</th>
+                ))}
+            </tr>
+        </thead>
+        <tbody>
+            {data.map((row, i) => (
+                <tr key={i}>
+                    {headers.map((header) => (
+                        <td key={header.key}>{row[header.key]}</td>
+                    ))}
+                </tr>
+            ))}
+        </tbody>
+    </table>
+);
+
+export default ModeratorSortableTable;
