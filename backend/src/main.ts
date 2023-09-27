2e3012d709bb250ef667c0ef6e53a87b4b94350e
Myloozy <myleshosken@Hotmail.com>
2023-09-27 15:08:44 +1300

    Update cors header

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(cors({ origin: true, credentials: true }));
   await app.listen(3000);
 }
 bootstrap();
