1e08211e0fc55864a250381602ac1ceb53c894c5
Myloozy <myleshosken@Hotmail.com>
2023-09-27 15:22:25 +1300

    Update createArticle.js

--- backend/api/articles/createArticle.js
+++ backend/api/articles/createArticle.js
@@ -6,6 +6,11 @@ export default async (req, res) => {
   res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Accept');
 
+    if (req.method === 'OPTIONS') {
+      res.status(200).end();
+      return;
+  }
+
   if (req.method === 'POST') {
     try {
       const db = await connectToDatabase();
