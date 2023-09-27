0fbab2c6d437b364b5244388d02c5ac93aad2d0f
Myloozy <myleshosken@Hotmail.com>
2023-09-27 15:10:27 +1300

    Removing app.use cors error

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,7 +3,6 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.use(cors({ origin: true, credentials: true }));
   await app.listen(3000);
 }
 bootstrap();
