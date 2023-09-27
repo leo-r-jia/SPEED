bf7752872d47477e1b4c8b5cabef69c11d1d77e3
Leo Jia <leo.r.jia@outlook.com>
2023-09-27 15:36:08 +1300

    Removed code smells

--- frontend/src/pages/articles/new.tsx
+++ frontend/src/pages/articles/new.tsx
@@ -1,6 +1,5 @@
 import { FormEvent, useState } from "react";
 import formStyles from "../../styles/Form.module.scss";
-import axios from 'axios';
 
 const NewDiscussion = () => {
     const [title, setTitle] = useState("");
