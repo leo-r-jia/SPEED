d918a775fd80bbbe56b7e8bd4ef97e213c4858f9
Leo Jia <leo.r.jia@outlook.com>
2023-10-02 20:11:53 +1300

    Started redesigning nav bar and new form page

--- frontend/src/components/PopulatedNavBar.tsx
+++ frontend/src/components/PopulatedNavBar.tsx
@@ -11,33 +11,19 @@ import NavItem from "./nav/NavItem";
 const PopulatedNavBar = () => {
 
     return (
-
         <NavBar>
-
             <NavItem>SPEED</NavItem>
-
             <NavItem route="/" end>
-
                 Home
-
             </NavItem>
-
             <NavItem dropdown route="/articles">
-
                 Articles <IoMdArrowDropdown />
-
                 <NavDropdown>
-
                     <NavItem route="/articles">View articles</NavItem>
-
                     <NavItem route="/articles/new">Submit new</NavItem>
-
                 </NavDropdown>
-
             </NavItem>
-
         </NavBar>
-
     );
 
 };
