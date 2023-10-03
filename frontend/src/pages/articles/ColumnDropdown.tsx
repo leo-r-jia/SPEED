12c1a788719786df98b6df0fb659d0a0ba5ff272
Leonic16246 <108039973+Leonic16246@users.noreply.github.com>
2023-10-03 21:21:35 +1300

    added import

--- frontend/src/pages/articles/ColumnDropdown.tsx
+++ frontend/src/pages/articles/ColumnDropdown.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import styles from "./ColumnDropdown.module.scss";
 
 
@@ -13,48 +14,47 @@ interface ColumnDropdownProps {
 }
 
 const ColumnDropdown: React.FC<ColumnDropdownProps> = ({
-  options,
-  selectedOptions,
-  onSelect,
-}) => {
-  const [isOpen, setIsOpen] = useState(false);
-
-  const toggleDropdown = () => {
-    setIsOpen(!isOpen);
+    options,
+    selectedOptions,
+    onSelect,
+  }) => {
+    const [isOpen, setIsOpen] = useState(false);
+  
+    const toggleDropdown = () => {
+      setIsOpen(!isOpen);
+    };
+  
+    const toggleOption = (optionKey: string) => {
+      const updatedSelected = selectedOptions.includes(optionKey)
+        ? selectedOptions.filter((key) => key !== optionKey)
+        : [...selectedOptions, optionKey];
+  
+      onSelect(updatedSelected);
+    };
+  
+    return (
+<div className={`${styles["column-dropdown"]} ${isOpen ? styles["open"] : ""}`}>
+  <div className={styles["dropdown-toggle"]} onClick={toggleDropdown}>
+    Select Columns
+  </div>
+  {isOpen && (
+    <ul className={styles["dropdown-options"]}>
+      {options.map((option) => (
+        <li
+          key={option.key}
+          onClick={() => toggleOption(option.key)}
+          className={`${selectedOptions.includes(option.key) ? styles["selected"] : ""}`}
+        >
+          {option.label}
+          {selectedOptions.includes(option.key) && (
+            <span className={styles["checkmark"]}>&#10003;</span>
+          )}
+        </li>
+      ))}
+    </ul>
+  )}
+</div>
+    );
   };
-
-  const toggleOption = (optionKey: string) => {
-    const updatedSelected = selectedOptions.includes(optionKey)
-      ? selectedOptions.filter((key) => key !== optionKey)
-      : [...selectedOptions, optionKey];
-
-    onSelect(updatedSelected);
-  };
-
-  return (
-    <div className={`${styles["column-dropdown"]} ${isOpen ? styles["open"] : ""}`}>
-      <div className={styles["dropdown-toggle"]} onClick={toggleDropdown}>
-        Select Columns
-      </div>
-      {isOpen && (
-        <ul className={styles["dropdown-options"]}>
-          {options.map((option) => (
-            <li
-              key={option.key}
-              className={`${selectedOptions.includes(option.key) ? styles["selected"] : ""}`}
-            >
-              <div onClick={() => toggleOption(option.key)}>
-                {option.label}
-                {selectedOptions.includes(option.key) && (
-                  <span className={styles["checkmark"]}>&#10003;</span>
-                )}
-              </div>
-            </li>
-          ))}
-        </ul>
-      )}
-    </div>
-  );
-};
-
-export default ColumnDropdown;
+  
+  export default ColumnDropdown;
