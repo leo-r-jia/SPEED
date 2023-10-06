5410b22f9be6e85629a2262f2ed344c529cc1a6b
Myles <Myleshosken@hotmail.com>
2023-10-07 12:05:09 +1300

    TDD tests

---
+++ backend/api/articles/isValidArticle.js
@@ -0,0 +1,11 @@
+
+function isValidArticle(title) {
+        if (title.length < 5 || title.length > 100) return false;
+    
+    const specialCharPattern = /[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]+/;
+    if (specialCharPattern.test(title)) return false;
+
+    return true;
+}
+
+export default isValidArticle;
