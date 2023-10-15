4c7ba6a6b13b4b6e12446866ddde3fa1389e27bb
Leo Jia <leo.r.jia@outlook.com>
2023-10-15 19:58:33 +1300

    Added to Frontend.test.tsx file

--- frontend/tests/unit/Frontend.test.tsx
+++ frontend/tests/unit/Frontend.test.tsx
@@ -1,7 +1,8 @@
 import { render, fireEvent, screen } from "@testing-library/react";
 import Articles from "../../src/pages/articles/moderator";
-import SortableTable from "../../src/components/table/SortableTable"; // Import the SortableTable component
+import SortableTable from "../../src/components/table/SortableTable";
 import ColumnDropdown from "../../src/pages/articles/ColumnDropdown"
+import NewArticle from '../../src/pages/articles/new';
 
 import "@testing-library/jest-dom";
 
@@ -89,6 +90,77 @@ describe("My Test Suite", () => {
     expect(screen.getByText("Test Article 2")).toBeInTheDocument();
     expect(screen.getByText("Author 3, Author 4")).toBeInTheDocument();
     expect(screen.getByText("2022")).toBeInTheDocument();
+  });
+
+  // Test 5: test the rendering of the NewArticle component
+  test('renders NewArticle component', () => {
+    render(<NewArticle />);
+
+    // Check if the form elements are present
+    const titleInput = screen.getByLabelText('Title:');
+    const sourceInput = screen.getByLabelText('Source:');
+    const pubYearInput = screen.getByLabelText('Publication Year:');
+    const doiInput = screen.getByLabelText('DOI:');
+    const sePracticeInput = screen.getByLabelText('Method/practice:');
+    const claimInput = screen.getByLabelText('Claim:');
+    const evidenceInput = screen.getByLabelText('Result of Evidence:');
+    const submitButton = screen.getByText('Submit');
+
+    expect(titleInput).toBeInTheDocument();
+    expect(sourceInput).toBeInTheDocument();
+    expect(pubYearInput).toBeInTheDocument();
+    expect(doiInput).toBeInTheDocument();
+    expect(sePracticeInput).toBeInTheDocument();
+    expect(claimInput).toBeInTheDocument();
+    expect(evidenceInput).toBeInTheDocument();
+    expect(submitButton).toBeInTheDocument();
+  });
+
+
+  // Test 6: tests form validation with empty fields
+  test('displays a warning message for empty fields', () => {
+    render(<NewArticle />);
+
+    const submitButton = screen.getByText('Submit');
+
+    // Submit the form without filling in any fields
+    fireEvent.click(submitButton);
+
+    // Check if the warning message is displayed
+    const warningMessage = screen.getByText('Please fill in all fields');
+    expect(warningMessage).toBeInTheDocument();
+  });
+
+  // Test 7: test to ensure input fields work
+  test('can change input values', () => {
+    render(<NewArticle />);
+
+    // Find the input elements
+    const titleInput = screen.getByLabelText('Title:') as HTMLInputElement;
+    const sourceInput = screen.getByLabelText('Source:') as HTMLInputElement;
+    const pubYearInput = screen.getByLabelText('Publication Year:') as HTMLInputElement;
+    const doiInput = screen.getByLabelText('DOI:') as HTMLInputElement;
+    const sePracticeInput = screen.getByLabelText('Method/practice:') as HTMLInputElement;
+    const claimInput = screen.getByLabelText('Claim:') as HTMLInputElement;
+    const evidenceInput = screen.getByLabelText('Result of Evidence:') as HTMLInputElement;
+
+    // Simulate user input
+    fireEvent.change(titleInput, { target: { value: 'Sample Title' } });
+    fireEvent.change(sourceInput, { target: { value: 'Sample Source' } });
+    fireEvent.change(pubYearInput, { target: { value: '2023' } });
+    fireEvent.change(doiInput, { target: { value: 'Sample DOI' } });
+    fireEvent.change(sePracticeInput, { target: { value: 'Sample Practice' } });
+    fireEvent.change(claimInput, { target: { value: 'Sample Claim' } });
+    fireEvent.change(evidenceInput, { target: { value: 'Sample Evidence' } });
+
+    // Check if the input values have changed
+    expect(titleInput.value).toBe('Sample Title');
+    expect(sourceInput.value).toBe('Sample Source');
+    expect(pubYearInput.value).toBe('2023');
+    expect(doiInput.value).toBe('Sample DOI');
+    expect(sePracticeInput.value).toBe('Sample Practice');
+    expect(claimInput.value).toBe('Sample Claim');
+    expect(evidenceInput.value).toBe('Sample Evidence');
   });
 
 });
